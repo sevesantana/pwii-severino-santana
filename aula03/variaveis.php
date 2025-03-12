42d36deee8daf03180f615bfe2062c6c664ba15d
Severino <snsf2134@gmail.com>
2025-03-12 10:16:45 -0300

    Gettype.

---
+++ aula03/variaveis.php
@@ -0,0 +1,13 @@
+<?php
+    $texto = "Isso é um texto";
+    $n1 = 2;
+    $n2 = 3.14;
+    $valor = null;
+
+    echo $texto;
+    echo "<br>";
+    echo gettype($texto), "<br>";
+    echo gettype($n1), "<br>";
+    echo gettype($n2), "<br>";
+    echo gettype($valor);
+?>

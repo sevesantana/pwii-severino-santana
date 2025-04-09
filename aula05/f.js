052c8c5cce3b69a68ae156156e60fa1f98b74248
Severino <snsf2134@gmail.com>
2025-04-09 10:32:51 -0300

    Aula sobre funções.

---
+++ aula05/f.js
@@ -0,0 +1,5 @@
+function Soma (n1, n2) {
+    console.log(n1 + n2)
+}
+
+Soma(4, 6)
---
+++ aula05/funcao.php
@@ -0,0 +1,20 @@
+<?php
+    function myMessage() {
+        echo "Hello world! <br>";
+    }
+
+    myMessage();
+    myMessage();
+
+    function Soma($n1, $n2) {
+        echo $n1+$n2, "<br>";
+    }
+
+    Soma(3, 4);
+    Soma(3, 10);
+
+    function cadastrar(){
+        //vai ser uma lógica para cadastrar
+    }
+    
+?> 

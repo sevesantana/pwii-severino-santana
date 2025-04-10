1178b4751013afa694ffb186744f890a1a3f4b47
Severino <snsf2134@gmail.com>
2025-04-10 13:47:20 -0300

    Aula sobre repetições em PHP.

---
+++ aula04/repeticao.php
@@ -0,0 +1,29 @@
+<?php  
+$i = 1;
+
+while ($i < 11) {
+  echo "<br>";
+  echo $i;
+  $i++;
+} 
+
+echo "<br> --------------- <br>";     
+
+$i = 1;
+
+do {
+    echo "<br>";
+  echo $i;
+  $i++;
+} while ($i < 11);
+
+echo "<br> --------------- <br>";     
+
+
+for ($x = 0; $x <= 10; $x++) {
+    echo "O Número é: $x <br>";
+  }
+
+
+
+?>  

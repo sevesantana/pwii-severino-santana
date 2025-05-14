f0dee6418e66e5c806c1bbd7ad66f9246781ba33
Severino <snsf2134@gmail.com>
2025-05-14 10:32:55 -0300

    Primeiros exercicios sobre PHP.

---
+++ exercicios/exercicios01/exercicio01.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Consumo médio: <?php echo $_GET ("distance")/$_GET ("fuel"); ?>
+</body>
+</html>

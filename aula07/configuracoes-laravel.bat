4cb0c987bbc30362443d3373fa75c93e9c45dde4
Severino <snsf2134@gmail.com>
2025-09-03 10:28:44 -0300

    Laravel.

---
+++ aula07/configuracoes-laravel.bat
@@ -0,0 +1,23 @@
+echo OFF
+
+echo ----- Instalar dependências composer -----
+call composer install
+
+echo ----- Instalar dependências npm -----
+call npm install
+
+
+
+echo ----- Rodar npm run build -----
+call npm run build
+
+echo ----- Gerar chave aleatória pelo Artisan -----
+call php artisan key:generate
+
+echo ----- Migrar banco de dados pelo Artisan -----
+call php artisan migrate
+
+echo ----- Rodar server do projeto -----
+call composer run dev
+
+pause
